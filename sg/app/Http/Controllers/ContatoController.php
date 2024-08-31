b0f93ca9a86418965861793542721df4b314cdb5
Ibercson Alves <103955886+nabucoanalista@users.noreply.github.com>
2024-08-30 23:31:56 -0300

    Super_gestao_valid

--- sg/app/Http/Controllers/ContatoController.php
+++ sg/app/Http/Controllers/ContatoController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request) {
+
+        //realizar a validação dos dados do formulário recebidos no request.
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        //SiteContato::create($request->all());
     }
 }
