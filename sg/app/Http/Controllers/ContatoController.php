147a51b17bd4ce3c0737c9bb28192d3d1446262c
Ibercson Alves <103955886+nabucoanalista@users.noreply.github.com>
2024-09-01 00:44:22 -0300

    novo comit

--- sg/app/Http/Controllers/ContatoController.php
+++ sg/app/Http/Controllers/ContatoController.php
@@ -9,7 +9,12 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
